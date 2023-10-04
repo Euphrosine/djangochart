0fa3d3f4fbe09bbf717cb78eb953c52ff89daf06
Euphrosine <euphrosine@46.com>
2023-10-04 16:54:36 +0200

    new codes

--- sensor_app/views.py
+++ sensor_app/views.py
@@ -14,20 +14,23 @@ class SensorDataAPI(APIView):
         data = SensorData.objects.all()
         response_data = [{'datetime': entry.datetime, 'activity': entry.activity, 'status': entry.status} for entry in data]
         return Response(response_data)
+    pass
 
-    def post(self, request):
-        status_code = request.data.get('status', 0)
+    def post(self, request, status_code=None):
+        if status_code is not None:
+            status_code = int(status_code)
 
-        if status_code == 1:
-            activity = "Unauthorized person trying to enter the car"
-        elif status_code == 11:
-            activity = "Unknown person trying to enter in the car"
+            if status_code == 1:
+                activity = "Unauthorized person trying to enter the car"
+            elif status_code == 11:
+                activity = "Unknown person trying to enter in the car"
+            else:
+                activity = "No activity"
+
+            SensorData.objects.create(datetime=datetime.now(), activity=activity, status=status_code)
+            return Response({'message': 'Data received successfully'}, status=status.HTTP_201_CREATED)
         else:
-            activity = "No activity"
-
-        SensorData.objects.create(datetime=datetime.now(), activity=activity,status=status_code)
-        return Response({'message': 'Data received successfully'}, status=status.HTTP_201_CREATED)
-
+            return Response({'error': 'Invalid status code'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 def sensor_data_view(request):
