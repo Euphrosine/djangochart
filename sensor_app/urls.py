8f0f491c8b37e4e4707e1cb6329878f5765f8b85
Euphrosine <euphrosine@46.com>
2023-10-04 15:34:17 +0200

    database reset

--- sensor_app/urls.py
+++ sensor_app/urls.py
@@ -3,6 +3,6 @@ from .views import SensorDataAPI, sensor_data_view
 
 urlpatterns = [
     path('api/sensor_data/', SensorDataAPI.as_view(), name='sensor_data_api'),
-    path('api/sensor_data/<int:status_code>/', SensorDataAPI.as_view(), name='sensor_data_api_status'),
+    path('api/sensor_data/<int:status>/', SensorDataAPI.as_view(), name='sensor_data_api_status'),
     path('sensor_data/', sensor_data_view, name='sensor_data_view'),
 ]
