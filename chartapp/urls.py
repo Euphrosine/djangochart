760072444a18dd03d27718ad38b5b0bff9c016f2
Euphrosine <euphrosine@46.com>
2023-10-09 15:28:10 +0300

    changes

--- chartapp/urls.py
+++ chartapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('chart_data/', views.chart_data_view, name='chart_data_view'),
-    path('dashboard/', views.dashboard_view, name='dashboard_view'),
+    path('', views.weather_data_view, name='index'),
 
 
 ]
--- chartapp/views.py
+++ chartapp/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from .models import WeatherData
 from django.utils import timezone
 
@@ -26,3 +27,35 @@ def chart_data_view(request):
 
     return JsonResponse({"message": "Data saved successfully"})
 
+
+
+def weather_data_view(request):
+    # Get the data for each field
+    temperature_data = WeatherData.objects.values_list('timestamp', 'temperature')
+    humidity_data = WeatherData.objects.values_list('timestamp', 'humidity')
+    rain_data = WeatherData.objects.values_list('timestamp', 'rain')
+    ldr_data = WeatherData.objects.values_list('timestamp', 'ldr')
+
+    # Prepare the data in a format suitable for Chart.js
+    temperature_labels = [entry[0].strftime('%H:%M:%S') for entry in temperature_data]
+    temperature_values = [entry[1] for entry in temperature_data]
+
+    humidity_labels = [entry[0].strftime('%H:%M:%S') for entry in humidity_data]
+    humidity_values = [entry[1] for entry in humidity_data]
+
+    rain_labels = [entry[0].strftime('%H:%M:%S') for entry in rain_data]
+    rain_values = [entry[1] for entry in rain_data]
+
+    ldr_labels = [entry[0].strftime('%H:%M:%S') for entry in ldr_data]
+    ldr_values = [entry[1] for entry in ldr_data]
+
+    return render(request, 'chartapp/index.html', {
+        'temperature_labels': temperature_labels,
+        'temperature_values': temperature_values,
+        'humidity_labels': humidity_labels,
+        'humidity_values': humidity_values,
+        'rain_labels': rain_labels,
+        'rain_values': rain_values,
+        'ldr_labels': ldr_labels,
+        'ldr_values': ldr_values,
+    })
