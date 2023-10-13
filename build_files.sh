e8c6f9ad1b493d8d3ade73481b5286461e4b4111
Euphrosine <euphrosine@46.com>
2023-10-13 11:05:03 +0300

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+# build_files.sh
 pip install -r requirements.txt
-python3.11 manage.py collectstatic
+
+# make migrations
+python manage.py migrate 
+python manage.py collectstatic
--- chart_django_project/urls.py
+++ chart_django_project/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from chartapp.views import generate_pdf_report
 
@@ -7,6 +9,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chartapp.urls')),
     path('api/', include('sensor_app.urls')),
-    path('generate_report/', generate_pdf_report, name='generate_pdf_report'),  
+    path('generate_report/', generate_pdf_report, name='generate_pdf_report'),
 ]
+    # add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
