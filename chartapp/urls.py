c6570320d09eaee9e183f0d6373d7f6f46dedcd7
Euphrosine <euphrosine46@.com>
2023-11-09 00:01:52 -0800

    changes

--- chartapp/urls.py
+++ chartapp/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('chart_data/', views.chart_data_view, name='chart_data_view'),
-    path('', views.weather_data_view, name='index'),
+    path('', views.overall_view, name='overall_page'),
+    path('charts/', views.weather_data_view, name='charts_page'),
     path('tables/', views.tables_view, name='tables_page'),
-    path('overall/', views.overall_view, name='overall_page'),
     path('register/', views.register, name='register'),
     path('login/', auth_view.LoginView.as_view(template_name='chartapp/login.html'), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='chartapp/logout.html'), name="logout"),
